d38f645d5c0dfbf491935e3169bcd0c53865e5b1
natesabrown <nateabrown@icloud.com>
2021-04-08 16:06:08 -0500

    get wikipedia information over https

--- src/util/wiki.js
+++ src/util/wiki.js
@@ -1,7 +1,9 @@
 import wiki from 'wikijs';
 
+const Wiki = wiki({ apiUrl: 'https://en.wikipedia.org/w/api.php'});
+
 export async function getSummary(name) {
-  const summary = await wiki().page(name).then(page => page.summary()).then(summary => {
+  const summary = await Wiki.page(name).then(page => page.summary()).then(summary => {
     const splitSummary = summary.split('\n')[0];
     const furtherSplit = splitSummary.split(". ");
     if (furtherSplit.length > 5) {
